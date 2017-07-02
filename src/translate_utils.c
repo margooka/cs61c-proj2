c1fb8602a9364a77b8be88aaafd4524b2f123455
MICHELLE ZHANG <MZHANG32@GMAIL.COM>
2017-07-01 18:44:53 -0700

    translate utils complete

--- src/translate_utils.c
+++ src/translate_utils.c
@@ -61,8 +61,13 @@ int translate_num(long int* output, const char* str, long int lower_bound,
     if (!str || !output) {
         return -1;
     }
-    /* YOUR CODE HERE */
-    return -1;
+    char* end;
+    long int sig_num = strtol(str, &end, 0);
+    if (*end || sig_num < lower_bound || sig_num > upper_bound) {
+        return -1;
+    }
+    *output = sig_num;
+    return 0;
 }
 
 /* Translates the register name to the corresponding register number. Please
@@ -76,6 +81,19 @@ int translate_reg(const char* str) {
     else if (strcmp(str, "$at") == 0)   return 1;
     else if (strcmp(str, "$v0") == 0)   return 2;
     else if (strcmp(str, "$a0") == 0)   return 4;
-    /* YOUR CODE HERE */
+    else if (strcmp(str, "$a1") == 0)   return 5;
+    else if (strcmp(str, "$a2") == 0)   return 6;
+    else if (strcmp(str, "$a3") == 0)   return 7;
+    else if (strcmp(str, "$t0") == 0)   return 8;
+    else if (strcmp(str, "$t1") == 0)   return 9;
+    else if (strcmp(str, "$t2") == 0)   return 10;
+    else if (strcmp(str, "$t3") == 0)   return 11;
+    else if (strcmp(str, "$s0") == 0)   return 16;
+    else if (strcmp(str, "$s1") == 0)   return 17;
+    else if (strcmp(str, "$s2") == 0)   return 18;
+    else if (strcmp(str, "$s3") == 0)   return 19;
+    else if (strcmp(str, "$sp") == 0)   return 29;
+    else if (strcmp(str, "$fp") == 0)   return 30;
+    else if (strcmp(str, "$ra") == 0)   return 31;
     else                                return -1;
 }
